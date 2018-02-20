2b2304a50f220699a6c244278433b77a32c72c64
Chiara Bigarella <chiarabigarella@it-laptop123.dyndns.cern.ch>
2018-02-20 11:15:05 +0100

    add header

--- gumption/src/app/app.module.ts
+++ gumption/src/app/app.module.ts
@@ -7,10 +7,16 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AppHeaderComponentComponent } from './app-header-component/app-header-component.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponentComponent,
+    HeaderComponent,
+    SearchboxComponent
   ],
   imports: [
     BsDropdownModule.forRoot(),
